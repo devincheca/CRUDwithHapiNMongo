1981416b51cb493f2df88eb6b8af57f0d97c1422
devincheca <devincheca@gmail.com>
2018-07-08 08:00:03 -0700

    updated node and now the static file server needs to be configured

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ const server = Hapi.server(
   port: 8000
 });
 
-server.route(
-{
-  method: 'GET',
-  path: '/{name}',
-  handler: (request, h) =>
-  {
-    return h.file('./public/{request.params.name}');
-  }
-});
-
 const init = async () =>
 {
   await server.register(require('inert'));
@@ -31,7 +21,7 @@ const init = async () =>
    }
   });
   await server.start();
-  console.log('Server running at: ${server.info.uri}');
+  console.log('Server running at: ' + server.info.uri);
 };
 
 process.on('unhandledRejection', (err) =>
